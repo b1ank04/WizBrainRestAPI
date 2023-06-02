9cd01cd3b23edaa204f3894bb3b78f92258094db
mark_shmarov <shmarov.mark@gmail.com>
2023-06-02 14:27:48 +0200

    Updates: Changed the summarization API

--- src/main/java/com/blank04/service/TextAnalyzeService.java
+++ src/main/java/com/blank04/service/TextAnalyzeService.java
@@ -22,15 +22,18 @@ public class TextAnalyzeService {
 
     public String summarize(String text) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://gpt-summarization.p.rapidapi.com/summarize"))
+                .uri(URI.create("https://text-analysis12.p.rapidapi.com/summarize-text/api/v1.1"))
                 .header("content-type", "application/json")
                 .header("X-RapidAPI-Key", "324078c6ccmshf30d54b878a4a11p1927f8jsn9128530ac437")
-                .header("X-RapidAPI-Host", "gpt-summarization.p.rapidapi.com")
-                .method("POST", HttpRequest.BodyPublishers.ofString(String.format("{\r%n    \"text\": \"%s\",\r%n    \"num_sentences\": 3\r%n}", text.replace("\n", "\\n"))))
+                .header("X-RapidAPI-Host", "text-analysis12.p.rapidapi.com")
+                .method("POST", HttpRequest.BodyPublishers.ofString(String.format("{\r%n    \"language\": \"english\",\r%n    \"summary_percent\": 10,\r%n    \"text\": \"%s\"\r%n}", text.replace("\n", " ").replace("\r", ""))))
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
         JSONObject responseObject = new JSONObject(response.body());
         String result = responseObject.getString("summary");
+        if (result.length() < 10) {
+            result = "The text you sent is too small to summarize";
+        }
         requestRepository.save(new Request(null, "SUMMARIZE_TEXT", text, result));
         return result;
     }
